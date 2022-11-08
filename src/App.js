36121e23ae120788041a7096962d00442a0a6f87
Rahul Gupta <FKOL015526@fusionbposervices.com>
2022-11-08 12:28:32 +0530

    first modification

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Scorecard from './components/Scorecard';
+import Transcriptions from './components/TranscriptionsPage';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        {/* <Route path='/transcriptions' element={<Transcriptions/>}/>
+        <Route path='/scorecard' element={<Scorecard/>}/> */}
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
